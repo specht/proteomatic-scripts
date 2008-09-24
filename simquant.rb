0bb5882b8adfa99d2bb947255066536c3aa3d155
Michael Specht <micha.specht@gmail.com>
2008-09-24 13:58:09 +0000

    adapted sim-quant.rb to new SimQuant with mean/stddev

--- simquant.rb
+++ simquant.rb
@@ -90,6 +90,16 @@ class SimQuant < ProteomaticScript
 				lk_Out.puts "for (var i = 0; i < lk_Elements.length; ++i)"
 				lk_Out.puts "lk_Elements[i].style.display = lk_Elements[i].style.display == 'none' ? 'table-row' : 'none';"
 				lk_Out.puts "}"
+				lk_Out.puts "function show(as_Name) {"
+				lk_Out.puts "lk_Elements = document.getElementsByClassName(as_Name);"
+				lk_Out.puts "for (var i = 0; i < lk_Elements.length; ++i)"
+				lk_Out.puts "lk_Elements[i].style.display = 'table-row';"
+				lk_Out.puts "}"
+				lk_Out.puts "function hide(as_Name) {"
+				lk_Out.puts "lk_Elements = document.getElementsByClassName(as_Name);"
+				lk_Out.puts "for (var i = 0; i < lk_Elements.length; ++i)"
+				lk_Out.puts "lk_Elements[i].style.display = 'none';"
+				lk_Out.puts "}"
 				lk_Out.puts "]]>"
 				lk_Out.puts "</script>"
 				lk_Out.puts '</head>'
@@ -99,7 +109,8 @@ class SimQuant < ProteomaticScript
 				lk_FoundPeptides = Array.new
 				lk_FoundPeptides = lk_Results['results'].keys if lk_Results['results']
 				lk_NotFoundPeptides = lk_Peptides.reject { |x| lk_FoundPeptides.include?(x) }
-				lk_Out.puts "Searched for #{lk_Peptides.size} peptide#{lk_Peptides.size != 1 ? 's' : ''} in #{@input[:spectra].size} file#{@input[:spectra].size != 1 ? 's' : ''}, trying charge states #{@param[:minCharge]} to #{@param[:maxCharge]} and merging the upper #{@param[:cropUpper]}% of all scans in which a peptide was found.<br />"
+				lk_Out.puts "Searched for #{lk_Peptides.size} peptide#{lk_Peptides.size != 1 ? 's' : ''} in #{@input[:spectra].size} file#{@input[:spectra].size != 1 ? 's' : ''}, trying charge states #{@param[:minCharge]} to #{@param[:maxCharge]} and merging the upper #{@param[:cropUpper]}% (SNR) of all scans in which a peptide was found.<br />"
+				lk_Out.puts "Quantitation has been attempted in all #{@param[:scanType].split(',').collect { |x| x == 'sim' ? 'SIM' : (x == 'full' ? 'FULL' : 'unknown')}.join(' and ')} scans, considering #{@param[:isotopeCount]} isotope peaks for both the unlabeled and the labeled ions.<br />"
 				lk_Out.puts "#{lk_FoundPeptides.size} of these peptides were quantified, #{lk_NotFoundPeptides.size} have not been found."
 				lk_Out.puts '</p>'
 				lk_Out.puts '<h2>Contents</h2>'
@@ -108,12 +119,17 @@ class SimQuant < ProteomaticScript
 				lk_Out.puts "<li><a href='#header-unidentified-peptides'>Unidentified peptides</a></li>"
 				lk_Out.puts '</ol>'
 				lk_Out.puts "<h2 id='header-quantified-peptides'>Quantified peptides</h2>"
-				lk_Out.puts "<table style='min-width: 820px;'>"
+				
 				lk_Peptides = lk_Results['results'].keys.sort
+				
+				lk_Out.puts "<span class='toggle' onclick=\"show('scans-all');\">[show all spectra]</span> "
+				lk_Out.puts "<span class='toggle' onclick=\"hide('scans-all');\">[hide all spectra]</span> <br />"
+
+				lk_Out.puts "<table style='min-width: 820px;'>"
 				li_StaticToggleCounter = 0
 				lk_Peptides.each do |ls_Peptide|
 					lk_Out.puts "<tr><td colspan='5' style='border: none; padding-top: 1em; padding-bottom: 1em;'><b>#{ls_Peptide}</b></td></tr>"
-					lk_Out.puts "<tr><th>Spot</th><th>Charge</th><th>Ratio</th><th>Certainty</th><th>Scans</th></tr>"
+					lk_Out.puts "<tr><th>Spot</th><th>Charge</th><th>Ratio mean (std. dev.)</th><th>SNR mean (std. dev.)</th><th>Scans</th></tr>"
 					lk_PeptideResults = lk_Results['results'][ls_Peptide]
 					lk_PeptideResults.sort! do |a, b|
 						(a['file'] == b['file']) ? 
@@ -123,16 +139,16 @@ class SimQuant < ProteomaticScript
 					lk_PeptideResults.each do |lk_Row|
 						lk_SubRows = lk_Row['scans']
 						lk_SubRows.sort! { |a, b| a['retentionTime'] <=> b['retentionTime'] }
-						li_StaticToggleCounter += 1
 						ls_Name = "scan-#{li_StaticToggleCounter}"
-						lk_Out.puts "<tr><td>#{lk_Row['file']}</td><td>#{lk_Row['charge']}</td><td>#{sprintf("%1.2f", lk_Row['ratio'].to_f)}</td><td>#{sprintf("%1.2f", lk_Row['certainty'].to_f * 100.0)} %</td><td><span class='toggle' onclick=\"toggle('#{ls_Name}')\">#{lk_SubRows.size} scans</span></td></tr>"
+						li_StaticToggleCounter += 1
+						lk_Out.puts "<tr><td>#{lk_Row['file']}</td><td>#{lk_Row['charge']}</td><td>#{sprintf("%1.2f (%1.2f)", lk_Row['ratioMean'].to_f, lk_Row['ratioStandardDeviation'].to_f)}</td><td>#{sprintf("%1.2f (%1.2f)", lk_Row['snrMean'].to_f, lk_Row['snrStandardDeviation'].to_f)}</td><td><span class='toggle' onclick=\"toggle('#{ls_Name}')\">#{lk_SubRows.size} scans</span></td></tr>"
 						lk_SubRows.each do |lk_SubRow|
 							ls_Svg = File::read(File::join(ls_SvgPath, lk_SubRow['svg'] + '.svg'))
 							ls_Svg.sub!(/<\?xml.+\?>/, '')
 							ls_Svg.sub!(/<svg width=\".+\" height=\".+\"/, "<svg ")
-							lk_Out.puts "<tr class='sub #{ls_Name}' style='display: none;'><td>#{lk_Row['file']}</td><td>#{lk_Row['charge']}</td><td>#{sprintf("%1.2f", lk_SubRow['ratio'].to_f)}</td><td>#{sprintf("%1.2f", lk_SubRow['certainty'].to_f * 100.0)} %</td><td></td></tr>"
-							lk_Out.puts "<tr class='sub #{ls_Name}' style='display: none;'><td colspan='5'>"
-							lk_Out.puts "<div>#{lk_Row['file']} ##{lk_SubRow['id']} @ #{sprintf("%1.2f", lk_SubRow['retentionTime'].to_f)} minutes</div>"
+							lk_Out.puts "<tr class='sub #{ls_Name} scans-all' style='display: none;'><td>#{lk_Row['file']}</td><td>#{lk_Row['charge']}</td><td>#{sprintf("%1.2f", lk_SubRow['ratio'].to_f)}</td><td>#{sprintf("%1.2f", lk_SubRow['snr'].to_f)}</td><td></td></tr>"
+							lk_Out.puts "<tr class='sub #{ls_Name} scans-all' style='display: none;'><td colspan='5'>"
+							lk_Out.puts "<div>#{lk_Row['file']} ##{lk_SubRow['id']} @ #{sprintf("%1.2f", lk_SubRow['retentionTime'].to_f)} minutes: #{lk_SubRow['filterLine']}</div>"
 							lk_Out.puts ls_Svg
 							lk_Out.puts "</td></tr>"
 						end
