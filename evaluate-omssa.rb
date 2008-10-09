40dea5d582cbd0dfdcabd1a6a7b55111f1bf4bb2
Michael Specht <micha.specht@gmail.com>
2008-10-09 13:03:05 +0000

    updated SimQuant, Evaluate OMSSA can write SimQuant peptides with protein info now

--- evaluate-omssa.rb
+++ evaluate-omssa.rb
@@ -276,12 +276,21 @@ class EvaluateOmssa < ProteomaticScript
 			end
 		end
 		
-		if (@output[:identifiedPeptides])
-			File.open(@output[:identifiedPeptides], 'w') do |lk_Out|
+		if (@output[:allIdentifiedPeptides])
+			File.open(@output[:allIdentifiedPeptides], 'w') do |lk_Out|
 				lk_Peptides = lk_PeptideHash.keys.sort
 				lk_Peptides.each do |ls_Peptide|
 					lk_Out.puts ">#{ls_Peptide}"
 					lk_Out.puts ls_Peptide
+				end
+			end
+		end
+		
+		if (@output[:simQuantPeptides])
+			File.open(@output[:simQuantPeptides], 'w') do |lk_Out|
+				lk_Peptides = lk_ProteinIdentifyingModelPeptides.to_a.sort
+				lk_Peptides.each do |ls_Peptide|
+					lk_Out.puts "#{ls_Peptide};#{lk_PeptideHash[ls_Peptide]['proteins'].keys.first}"
 				end
 			end
 		end
--- simquant.rb
+++ simquant.rb
@@ -32,19 +32,9 @@ class SimQuant < ProteomaticScript
 		lk_Peptides = @param[:peptides].split(%r{[,;\s/]+})
 		lk_Peptides.reject! { |x| x.strip.empty? }
 		
-		@input[:peptidesFile].each do |ls_Path|
-			next unless fileMatchesFormat(ls_Path, 'txt')
-			ls_File = File::read(ls_Path)
-			ls_File.each do |ls_Line|
-				ls_Peptide = ls_Line.strip
-				next if ls_Peptide.empty?
-				lk_Peptides.push(ls_Peptide)
-			end
-		end
-		
 		lk_Peptides.uniq!
 		lk_Peptides.collect! { |x| x.upcase }
-		if lk_Peptides.empty?
+		if lk_Peptides.empty? && @input[:peptideFiles].empty?
 			puts 'Error: no peptides have been specified.'
 			exit 1
 		end
@@ -55,7 +45,7 @@ class SimQuant < ProteomaticScript
 		FileUtils::mkpath(ls_TempPath)
 		FileUtils::mkpath(ls_SvgPath)
 		
-		ls_Command = "\"#{ExternalTools::binaryPath('simquant.simquant')}\" --scanType #{@param[:scanType]} --isotopeCount #{@param[:isotopeCount]} --cropUpper #{@param[:cropUpper] / 100.0} --minSnr #{@param[:minSnr]} --maxOffCenter #{@param[:maxOffCenter] / 100.0} --maxTimeDifference #{@param[:maxTimeDifference]} --textOutput no --yamlOutput yes --yamlOutputTarget \"#{ls_YamlPath}\" --svgOutPath \"#{ls_SvgPath}\" --files #{@input[:spectra].collect {|x| '"' + x + '"'}.join(' ')} --peptides #{lk_Peptides.join(' ')}"
+		ls_Command = "\"#{ExternalTools::binaryPath('simquant.simquant')}\" --scanType #{@param[:scanType]} --isotopeCount #{@param[:isotopeCount]} --cropUpper #{@param[:cropUpper] / 100.0} --minSnr #{@param[:minSnr]} --maxOffCenter #{@param[:maxOffCenter] / 100.0} --maxTimeDifference #{@param[:maxTimeDifference]} --textOutput no --yamlOutput yes --yamlOutputTarget \"#{ls_YamlPath}\" --svgOutPath \"#{ls_SvgPath}\" --files #{@input[:spectra].collect {|x| '"' + x + '"'}.join(' ')} --peptides #{lk_Peptides.join(' ')} --peptideFiles #{@input[:peptideFiles].collect {|x| '"' + x + '"'}.join(' ')}"
 		puts 'There was an error while executing simquant.' unless system(ls_Command)
 		
 		lk_Results = YAML::load_file(ls_YamlPath)
@@ -113,59 +103,58 @@ class SimQuant < ProteomaticScript
 				lk_FoundPeptides = Array.new
 				lk_FoundPeptides = lk_Results['results'].keys if lk_Results['results']
 				lk_NotFoundPeptides = lk_Peptides.reject { |x| lk_FoundPeptides.include?(x) }
-				lk_Out.puts "Searched for #{lk_Peptides.size} peptide#{lk_Peptides.size != 1 ? 's' : ''} in #{@input[:spectra].size} file#{@input[:spectra].size != 1 ? 's' : ''}, trying charge states #{@param[:minCharge]} to #{@param[:maxCharge]} and merging the upper #{@param[:cropUpper]}% (SNR) of all scans in which a peptide was found.<br />"
-				lk_Out.puts "Quantitation has been attempted in all #{@param[:scanType].split(',').collect { |x| x == 'sim' ? 'SIM' : (x == 'full' ? 'Full' : 'unknown')}.join(' and ')} scans, considering #{@param[:isotopeCount]} isotope peaks for both the unlabeled and the labeled ions.<br />"
-				lk_Out.puts "#{lk_FoundPeptides.size} of these peptides were quantified, #{lk_NotFoundPeptides.size} have not been found."
+				lk_Out.puts "Quantified #{lk_FoundPeptides.size} peptides in #{@input[:spectra].size} file#{@input[:spectra].size != 1 ? 's' : ''}, trying charge states #{@param[:minCharge]} to #{@param[:maxCharge]} and merging the upper #{@param[:cropUpper]}% (SNR) of all scans in which a peptide was found.<br />"
+				lk_Out.puts "Quantitation has been attempted in #{@param[:scanType] == 'sim' ? 'SIM' : 'all'} scans, considering #{@param[:isotopeCount]} isotope peaks for both the unlabeled and the labeled ions.<br />"
 				lk_Out.puts '</p>'
-				lk_Out.puts '<h2>Contents</h2>'
-				lk_Out.puts '<ol>'
-				lk_Out.puts "<li><a href='#header-quantified-peptides'>Quantified peptides</a></li>"
-				lk_Out.puts "<li><a href='#header-unidentified-peptides'>Unidentified peptides</a></li>"
-				lk_Out.puts '</ol>'
-				lk_Out.puts "<h2 id='header-quantified-peptides'>Quantified peptides</h2>"
+				
+				lk_Proteins = Hash.new
+				lk_Results['results'].keys.each do |ls_Peptide|
+					ls_Protein = ''
+					ls_Protein = lk_Results['results'][ls_Peptide]['protein'] if lk_Results['results'][ls_Peptide]['protein']
+					ls_Protein = '(no protein specified)' if ls_Protein.empty?
+					lk_Proteins[ls_Protein] ||= Array.new
+					lk_Proteins[ls_Protein].push(ls_Peptide)
+				end
 				
 				lk_Peptides = lk_Results['results'].keys.sort
+				
+				lk_Out.puts "<h2>Quantified proteins</h2>"
 				
 				lk_Out.puts "<span class='toggle' onclick=\"show('scans-all');\">[show all spectra]</span> "
 				lk_Out.puts "<span class='toggle' onclick=\"hide('scans-all');\">[hide all spectra]</span> <br />"
 
 				lk_Out.puts "<table style='min-width: 820px;'>"
 				li_StaticToggleCounter = 0
-				lk_Peptides.each do |ls_Peptide|
-					lk_Out.puts "<tr><td colspan='5' style='border: none; padding-top: 1em; padding-bottom: 1em;'><b>#{ls_Peptide}</b></td></tr>"
-					lk_Out.puts "<tr><th>Spot</th><th>Charge</th><th>Ratio mean (std. dev.)</th><th>SNR mean (std. dev.)</th><th>Scans</th></tr>"
-					lk_PeptideResults = lk_Results['results'][ls_Peptide]
-					lk_PeptideResults.sort! do |a, b|
-						(a['file'] == b['file']) ? 
-							a['charge'].to_i <=> b['charge'].to_i :
-							String::natcmp(a['file'], b['file'])
-					end
-					lk_PeptideResults.each do |lk_Row|
-						lk_SubRows = lk_Row['scans']
-						lk_SubRows.sort! { |a, b| a['retentionTime'] <=> b['retentionTime'] }
-						ls_Name = "scan-#{li_StaticToggleCounter}"
-						li_StaticToggleCounter += 1
-						lk_Out.puts "<tr><td>#{lk_Row['file']}</td><td>#{lk_Row['charge']}</td><td>#{sprintf("%1.2f (%1.2f)", lk_Row['ratioMean'].to_f, lk_Row['ratioStandardDeviation'].to_f)}</td><td>#{cutMax(lk_Row['snrMean'].to_f, 10000, 2)} (#{cutMax(lk_Row['snrStandardDeviation'].to_f, 10000, 2)})</td><td><span class='toggle' onclick=\"toggle('#{ls_Name}')\">#{lk_SubRows.size} scans</span></td></tr>"
-						lk_SubRows.each do |lk_SubRow|
-							ls_Svg = File::read(File::join(ls_SvgPath, lk_SubRow['svg'] + '.svg'))
-							ls_Svg.sub!(/<\?xml.+\?>/, '')
-							ls_Svg.sub!(/<svg width=\".+\" height=\".+\"/, "<svg ")
-							lk_Out.puts "<tr class='sub #{ls_Name} scans-all' style='display: none;'><td>#{lk_Row['file']}</td><td>#{lk_Row['charge']}</td><td>#{sprintf("%1.2f", lk_SubRow['ratio'].to_f)}</td><td>#{cutMax(lk_SubRow['snr'].to_f, 10000, 2)}</td><td></td></tr>"
-							lk_Out.puts "<tr class='sub #{ls_Name} scans-all' style='display: none;'><td colspan='5'>"
-							lk_Out.puts "<div>#{lk_Row['file']} ##{lk_SubRow['id']} @ #{sprintf("%1.2f", lk_SubRow['retentionTime'].to_f)} minutes: #{lk_SubRow['filterLine']}</div>"
-							lk_Out.puts ls_Svg
-							lk_Out.puts "</td></tr>"
+				lk_ProteinKeys = lk_Proteins.keys.sort { |a, b| String::natcmp(a, b) }
+				
+				lk_ProteinKeys.each do |ls_Protein|
+					lk_Peptides = lk_Proteins[ls_Protein].sort
+					lk_Out.puts "<tr><td colspan='4' style='border: none; padding-top: 1em; padding-bottom: 1em;'><b>#{ls_Protein}</b></td></tr>"
+					lk_Peptides.each do |ls_Peptide|
+						lk_Out.puts "<tr><td colspan='4' style='border: none;'>#{ls_Peptide}</td></tr>"
+						lk_Out.puts "<tr><th>Spot</th><th>Ratio mean (std. dev.)</th><th>SNR mean (std. dev.)</th><th>Scans</th></tr>"
+						lk_PeptideResults = lk_Results['results'][ls_Peptide]['results']
+						lk_PeptideResults.sort! { |a, b| String::natcmp(a['file'], b['file']) }
+						lk_PeptideResults.each do |lk_Row|
+							lk_SubRows = lk_Row['scans']
+							lk_SubRows.sort! { |a, b| a['retentionTime'] <=> b['retentionTime'] }
+							ls_Name = "scan-#{li_StaticToggleCounter}"
+							li_StaticToggleCounter += 1
+							lk_Out.puts "<tr><td>#{lk_Row['file']}</td><td>#{sprintf("%1.2f (%1.2f)", lk_Row['ratioMean'].to_f, lk_Row['ratioStandardDeviation'].to_f)}</td><td>#{cutMax(lk_Row['snrMean'].to_f, 10000, 2)} (#{cutMax(lk_Row['snrStandardDeviation'].to_f, 10000, 2)})</td><td><span class='toggle' onclick=\"toggle('#{ls_Name}')\">#{lk_SubRows.size} scans</span></td></tr>"
+							lk_SubRows.each do |lk_SubRow|
+								ls_Svg = File::read(File::join(ls_SvgPath, lk_SubRow['svg'] + '.svg'))
+								ls_Svg.sub!(/<\?xml.+\?>/, '')
+								ls_Svg.sub!(/<svg width=\".+\" height=\".+\"/, "<svg ")
+								lk_Out.puts "<tr class='sub #{ls_Name} scans-all' style='display: none;'><td>#{lk_Row['file']}</td><td>#{sprintf("%1.2f", lk_SubRow['ratio'].to_f)}</td><td>#{cutMax(lk_SubRow['snr'].to_f, 10000, 2)}</td><td></td></tr>"
+								lk_Out.puts "<tr class='sub #{ls_Name} scans-all' style='display: none;'><td colspan='4'>"
+								lk_Out.puts "<div>#{lk_Row['file']} ##{lk_SubRow['id']} @ #{sprintf("%1.2f", lk_SubRow['retentionTime'].to_f)} minutes: charge: #{lk_SubRow['charge']}+ / #{lk_SubRow['filterLine']}</div>"
+								lk_Out.puts ls_Svg
+								lk_Out.puts "</td></tr>"
+							end
 						end
 					end
 				end
 				lk_Out.puts '</table>'
-				lk_Out.puts "<h2 id='header-unidentified-peptides'>Unidentified peptides</h2>"
-				lk_Out.puts "<table><tr><th>Peptide</th></tr>"
-				lk_NotFoundPeptides.sort!
-				lk_NotFoundPeptides.each do |ls_Peptide|
-					lk_Out.puts "<tr><td>#{ls_Peptide}</td></tr>"
-				end
-				lk_Out.puts "</table>"
 				lk_Out.puts '</body>'
 				lk_Out.puts '</html>'
 			end
