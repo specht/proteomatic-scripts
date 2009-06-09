ca51697bcd9592c48e6af8e49c280e5e4488b42f
Michael Specht <micha.specht@gmail.com>
2009-06-09 13:58:34 +0000

    removed some tabs and added tab test

--- tests/run-tests.rb
+++ tests/run-tests.rb
@@ -4,7 +4,6 @@ $gs_Ruby = File.join(Config::CONFIG["bindir"], Config::CONFIG["ruby_install_name
 
 puts "Using #{$gs_Ruby}..."
 
-lk_Scripts = Dir[File::join(File::dirname($0), '..', '*.rb')]
 
 def testCommand(as_Path, as_Command)
 	ls_Command = "#{$gs_Ruby} -C#{File::dirname(as_Path)} #{File::basename(as_Path)} #{as_Command} > run-tests.temp.txt"
@@ -16,6 +15,18 @@ def testCommand(as_Path, as_Command)
 	end
 end
 
+puts 'Checking descriptions...'
+lk_Descriptions = Dir[File::join(File::dirname($0), '../include/properties/**', '*.yaml')]
+lk_Descriptions.each do |ls_Path|
+	ls_Content = File::read(ls_Path)
+	if ls_Content.include?("\t")
+		puts "YAML error: #{ls_Path} contains one or more tabs."
+		exit 1
+	end
+end
+
+puts 'Checking scripts...'
+lk_Scripts = Dir[File::join(File::dirname($0), '..', '*.rb')]
 lk_Scripts.sort.each do |ls_Path|
 	next if ls_Path.include?('.defunct.')
 	print "#{File::basename(ls_Path)}: "
