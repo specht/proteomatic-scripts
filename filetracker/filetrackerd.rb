374e4a030bc609e94f5978b1c3982ec0258c071c
Michael Specht <micha.specht@gmail.com>
2009-09-08 08:39:16 +0000

    tried to fix filetracker daemon
    replaced mutexes with monitors
    hardened session io (though some more could be done)

--- filetracker/filetrackerd.rb
+++ filetracker/filetrackerd.rb
@@ -2,8 +2,11 @@ require 'socket'
 require 'filetrackerhub'
 require 'mysql'
 require 'thread'
+require 'monitor'
+require 'timeout'
 
 
+# check whether gzip is there
 gzipVersion = `gzip -V`
 unless gzipVersion.index('gzip') == 0
 	puts "Error: gzip is not installed."
@@ -28,15 +31,78 @@ serverPort = ARGV[1].to_i if ARGV[1]
 puts "Starting server at #{serverHost}:#{serverPort}..."
 server = TCPServer.new(serverHost, serverPort);
 
-$databaseMutex = Mutex.new
-$fileMutex = Mutex.new
+# $databaseMutex = Mutex.new
+# $fileMutex = Mutex.new
+$fileMonitor = Monitor.new
+$databaseMonitor = Monitor.new
 
-while (session = server.accept)
-	Thread.new(session) do |session|
-		path = ''
-		
-		input = session.gets
-		
+
+def expectString(session, s)
+	offset = 0
+	s.each_byte do |b|
+		c = session.getc
+		return false if b != c
+	end
+	return true
+end
+
+
+while (newSession = server.accept)
+	Thread.new(newSession) do |session|
+		Timeout::timeout(30) do
+			path = ''
+			unless expectString(session, "PROTEOMATIC_FILETRACKER_REPORT\n")
+				session.close
+				Thread.exit
+			end
+			unless expectString(session, "VERSION 1\n")
+				session.close
+				Thread.exit
+			end
+			unless expectString(session, "LENGTH ")
+				session.close
+				Thread.exit
+			end
+			# read at most 7 digits (9999999 bytes max.)
+			lengthString = ''
+			7.times do
+				b = session.getc
+				c = b.chr()
+				break unless (b >= ?0) && (b <= ?9)
+				lengthString += c
+			end
+			length = lengthString.to_i
+
+			yamlReport = session.read(length)
+			
+			#puts "Received new report:"
+			#puts yamlReport
+			
+			session.puts "REPORT RECEIVED"
+			
+			# add report to database
+			reportData = YAML::load(yamlReport)
+			$databaseMonitor.synchronize do
+				addReport(conn, reportData)
+			end
+			puts "Report committed at #{Time.now.to_s}."
+			
+			session.puts "REPORT COMMITTED"
+			
+			# archive report
+			timestamp = Time.now.strftime("%Y-%m")
+			currentArchiveFilename = "filetracker-reports-#{timestamp}.yaml"
+			$fileMonitor.synchronize do
+				File.open("archive/#{currentArchiveFilename}", "a") do |f|
+					f.puts yamlReport
+				end
+				unarchivedFiles = Dir['archive/*.yaml']
+				unarchivedFiles.each do |path|
+					next if File::basename(path).include?(currentArchiveFilename)
+					system("gzip \"#{path}\"")
+				end
+			end
+=begin
 		if input.strip == 'PROTEOMATIC_FILETRACKER_REPORT'
 			input = session.gets
 			if input.strip == 'VERSION 1'
@@ -44,38 +110,10 @@ while (session = server.accept)
 				if input.index('LENGTH ') == 0
 					length = input.strip.sub('LENGTH ', '').to_i
 					
-					yamlReport = session.read(length)
-					
-					puts "Received new report:"
-					puts yamlReport
-					
-					session.puts "REPORT RECEIVED"
-					
-					# add report to database
-					reportData = YAML::load(yamlReport)
-					$databaseMutex.synchronize do
-						addReport(conn, reportData)
-					end
-					puts "Report committed."
-					puts
-					
-					session.puts "REPORT COMMITTED"
-					
-					# archive report
-					timestamp = Time.now.strftime("%Y-%m")
-					currentArchiveFilename = "filetracker-reports-#{timestamp}.yaml"
-					$fileMutex.synchronize do
-						File.open("archive/#{currentArchiveFilename}", "a") do |f|
-							f.puts yamlReport
-						end
-						unarchivedFiles = Dir['archive/*.yaml']
-						unarchivedFiles.each do |path|
-							next if File::basename(path).include?(currentArchiveFilename)
-							system("gzip \"#{path}\"")
-						end
-					end
 				end
 			end
+		end
+=end
 		end
 		session.flush
 		session.close
