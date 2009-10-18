b63db4142b1a0b3311024304bda2b2be34abc9cd
Michael Specht <micha.specht@gmail.com>
2009-10-18 21:40:07 +0200

    added add-fingerprints.rb

---
+++ add-fingerprints.rb
@@ -0,0 +1,54 @@
+# Copyright (c) 2009 Michael Specht
+# 
+# This file is part of Proteomatic.
+# 
+# Proteomatic is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# Proteomatic is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with Proteomatic.  If not, see <http://www.gnu.org/licenses/>.
+
+require 'include/proteomatic'
+require 'include/externaltools'
+require 'include/misc'
+require 'yaml'
+require 'fileutils'
+
+class AddFingerprints < ProteomaticScript
+
+	def run()
+		lk_Sum = Hash.new
+		@input[:fingerprints].each do |inPath|
+			File::open(inPath) do |lk_In|
+				headerLine = lk_In.readline
+				header = mapCsvHeader(headerLine)
+				lk_In.each_line do |line|
+					lineArray = line.parse_csv()
+					key = "#{lineArray[header['a']].to_i}/#{lineArray[header['d']].to_i}"
+					amount = lineArray[header['amount']].to_f
+					lk_Sum[key] ||= 0.0
+					lk_Sum[key] += amount
+				end
+			end
+		end
+		if @output[:sumFingerprint]
+			File::open(@output[:sumFingerprint], 'w') do |lk_Out|
+				lk_Out.puts "Amount,A,D"
+				lk_Sum.keys.each do |key|
+					a = key.split('/')[0]
+					d = key.split('/')[1]
+					lk_Out.puts "#{lk_Sum[key]},#{a},#{d}"
+				end
+			end
+		end
+	end
+end
+
+lk_Object = AddFingerprints.new
