f60e4e3d538a7e1df07aa0e339128c4d98aa2b6b
Michael Specht <micha.specht@gmail.com>
2010-04-28 11:40:22 +0200

    crosslink-masscalc script update

--- crosslink-masscalc.rb
+++ crosslink-masscalc.rb
@@ -22,29 +22,85 @@ require 'set'
 
 
 class CrossLinkMassCalc < ProteomaticScript
+    def fragmentMasses(peptide, cutIndex)
+        nTerm = peptide[0, cutIndex]
+        cTerm = peptide[cutIndex, peptide.size - cutIndex]
+        if cutIndex == peptide.size
+            return peptideMass(nTerm), peptideMass(cTerm)
+        else
+            return peptideMass(nTerm) - @waterMass, peptideMass(cTerm)
+        end
+    end
+    
+    def fragmentTitles(peptide, cutIndex)
+        nTerm = peptide[0, cutIndex]
+        cTerm = peptide[cutIndex, peptide.size - cutIndex]
+        return cutIndex == peptide.size ? "(complete)" : "b#{cutIndex}", 
+               cutIndex == 0 ? "(complete)" : "y#{peptide.size - cutIndex}"
+    end
+    
 	def run()
+        @hydrogenMass = $proteomicsKnowledge[:isotopes]['H'][:default][:monoisotopicmass]
+        @oxygenMass = $proteomicsKnowledge[:isotopes]['O'][:default][:monoisotopicmass]
+        @waterMass = @hydrogenMass * 2.0 + @oxygenMass
         crossLinkerMassShift = @param[:crossLinkerMassShift].to_f
         peptides = [@param[:peptideA], @param[:peptideB]]
-        cutIndices = [[], []]
+        linkIndices = [[], []]
         peptides.each_with_index do |peptide, i|
             lastIndex = -1
             while (lastIndex = peptide.index('K', lastIndex + 1))
-                cutIndices[i] << lastIndex
+                linkIndices[i] << lastIndex
             end
         end
-        # determine all possible crosslink combinations
-        cutIndices[0].each do |c0|
-            cutIndices[1].each do |c1|
-                combinationKey = "#{peptides[0]}-#{peptides[1]}-a#{c0 + 1}-b#{c1 + 1}"
-                puts combinationKey
-            end
-        end
+
+        # DSS is C8H10O2 (138.068080)
+        
+        # determine precursor mass
         precursorMass = peptideMass(peptides[0]) + peptideMass(peptides[1]) + crossLinkerMassShift
-        puts "Precursor mass: #{precursorMass}"
-        (1..3).each do |charge|
-            mz = (precursorMass + $proteomicsKnowledge[:isotopes]['H'][:default][:monoisotopicmass] * charge) / charge
-            puts mz
+        cutIndices = [(0..peptides[0].size).to_a, (0..peptides[1].size).to_a]
+        allMz = Array.new
+        # determine all possible crosslink combinations
+        linkIndices[0].each do |l0|
+            linkIndices[1].each do |l1|
+                l = [l0, l1]
+                combinationKey = "#{peptides[0]}-#{peptides[1]}-a#{l0 + 1}-b#{l1 + 1}"
+                puts combinationKey
+                (@param[:minCharge]..@param[:maxCharge]).each do |charge|
+                    precursorMz = (precursorMass + $proteomicsKnowledge[:isotopes]['H'][:default][:monoisotopicmass] * charge) / charge
+                    # calculate single peptide + crosslink masses
+                    # calculate crosslinked / dangling masses
+                    cutIndices[0].each do |c0|
+                        cutIndices[1].each do |c1|
+                            c = [c0, c1]
+                            # linkedMass is the fragment containing the crosslinker
+                            danglingMasses = [0.0, 0.0]
+                            title = ['', '']
+                            linkedMass = crossLinkerMassShift
+                            linkedTitle = []
+                            (0..1).each do |x|
+                                masses = fragmentMasses(peptides[x], c[x])
+                                titles = fragmentTitles(peptides[x], c[x])
+                                if (c[x] > l[x])
+                                    # crosslinker is on the N-terminal side
+                                    linkedMass += masses[0]
+                                    danglingMasses[x] = masses[1]
+                                    title[x] = titles[1]
+                                    linkedTitle << "#{x == 0 ? 'A' : 'B'} #{titles[0]}"
+                                else
+                                    danglingMasses[x] = masses[0]
+                                    linkedMass += masses[1]
+                                    title[x] = titles[0]
+                                    linkedTitle << "#{x == 0 ? 'A' : 'B'} #{titles[1]}"
+                                end
+                                allMz << [(danglingMasses[x] + @hydrogenMass * charge) / charge, "#{combinationKey} / dangling #{x == 0 ? 'A' : 'B'} #{title[x]} (#{charge}+)"] if title[x][-1, 1] != '0'
+                            end
+                            allMz << [(linkedMass + @hydrogenMass * charge) / charge, "#{combinationKey} / crosslinked #{linkedTitle.join(', ')} (#{charge}+)"]
+                        end
+                    end
+                end
+            end
         end
+        puts allMz.sort { |a, b| a[0] <=> b[0] }.collect { |x| sprintf('%9.4f', x[0]) + ': ' + x[1] }.uniq.join("\n")
 	end
 end
 
