3c7d5d58c9a3d0800cd861b69f29042d45b51cf7
Michael Specht <micha.specht@gmail.com>
2010-06-16 14:50:54 +0200

    small fix

--- helper/get-unresolved-dependencies.rb
+++ helper/get-unresolved-dependencies.rb
@@ -5,6 +5,7 @@ result = Hash.new
 
 ARGV.each do |script|
     descriptionPath = "include/properties/#{script.sub('.defunct.', '.').sub('.rb', '')}.yaml"
+    next unless File::exists?(descriptionPath)
     object = ProteomaticScript.new(descriptionPath)
     next unless object.configOk()
     result.merge!(object.unresolvedDependencies())
