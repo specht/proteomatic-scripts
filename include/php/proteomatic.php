f6855f55b04c005b557befebb9baf1a3bf87e52f
Michael Specht <micha.specht@gmail.com>
2010-07-07 15:48:41 +0200

    even more

--- include/php/proteomatic.php
+++ include/php/proteomatic.php
@@ -1,47 +1,84 @@
 <?php
 
-require_once ('include/php/ext/spyc.php');
-
 abstract class ProteomaticScript
 {
     abstract protected function run();
 	
-	protected $parameters;
+	protected $param;
+    protected $input;
+    protected $output;
     
     function __construct()
     {
-        global $yamlInfo;
         global $argv;
         
-        $scriptInfo = Spyc::YAMLLoad($yamlInfo);
-		
-		/*
-		parameters:
-		  key: value
-		*/
-		$this->parameters = Array();
-		$this->parameters['outfileformat'] = 'NEXUS';
-		
-		/*
-		inputFiles:
-		  groupKey: [path 1, path 2]
-		*/
-		$this->inputFiles = Array();
-		
-		/*
-		outputFiles:
-		  aln: path
-		*/
-		$this->inputFiles = Array();
-        // handle ---yamlInfo switch
-        if ($argv[1] == '---yamlInfo')
+        // remove script name from arg list
+        $scriptFilename = array_shift($argv);
+        $currentDir = getcwd();
+        
+        // remove $currentDir from $scriptFilename if it's a prefix
+        if ((count($currentDir) > 0) && (strpos($scriptFilename, $currentDir) == 0))
         {
-            echo "---yamlInfo\n";
-            echo $yamlInfo;
-            exit(0);
+            $scriptFilename = str_replace($currentDir, "", $scriptFilename);
         }
         
+        // change working directory to script's directory
+        $completeScriptPath = realpath($currentDir."/".$scriptFilename);
+        
+        chdir(dirname($completeScriptPath));
+        $scriptFilename = basename($scriptFilename);
+        
+        // check for user-defined Ruby
+        $pathToRuby = "ruby";
+        if (($index = array_search('--pathToRuby', $argv)) != FALSE)
+        {
+            $chunk = array_splice($argv, $index, 2);
+            $pathToRuby = $chunk[1];
+        }
+
+        // determine path to YAML description
+        $parts = explode('.', $scriptFilename);
+        $scriptBasename = implode('.', array_slice($parts, 0, count($parts) - 1));
+        $pathToYamlDescription = realpath("include/properties/$scriptBasename.yaml");
+
+        // create parameter string
+        $argString = "";
+        foreach ($argv as $arg)
+            $argString .= " \"$arg\"";
+
+        // call Proteomatic's any language hub
+        $lines = Array();
+        $commandLine = "\"$pathToRuby\" helper/any-language-hub.rb \"$pathToYamlDescription\" $argString";
+//         echo $commandLine."\n";
+        $process = popen($commandLine, 'r');
+        while (!feof($process))
+        {
+            $a = Array($process);
+            $b = NULL;
+            $c = NULL;
+            if (false === ($changedStreamCount = stream_select($a, $b, $c, 30)))
+            {
+                echo "Error!\n";
+            }
+            else
+            {
+//                 echo $changedStreamCount."\n";
+//                 echo fgets($process);
+                echo fread($process, 10);
+            }
+        }
+        pclose($process);
+
+        /*
+		$this->param = Array();
+		$this->input = Array();
+		$this->output = Array();
+
+        $this->param['decoyEntryPrefix'] = '__putative__';
+
         $this->run();
+        */
     }
 }
+
 ?>
