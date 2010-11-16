5ceb61ae11f4d35f16c932aa7eeee8286b1933cc
Christian Fufezan <christian@fufezan.net>
2010-11-16 14:50:55 +0100

    updated python interface added decoyfasta py version added *pyc to .gitignore

---
+++ decoyfasta_py.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python2.6
+"""
+Copyright (c) 2010 Christian Fufezan
+
+This file is part of Proteomatic.
+
+Proteomatic is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Proteomatic is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Proteomatic.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+import sys, os
+
+sys.path.append('./include/python')
+import proteomatic
+
+
+class DecoyFasta(proteomatic.ProteomaticScript):
+    def run(self):
+        if 'outputDatabase' in self.output.keys():
+            print("Creating taget/decoy database ... \n")
+            #print(self.output)
+            databases = " ".join(["\"{0}\"".format(x) for x in self.input['databases']])
+            opts = {
+            '--output':         self.output['outputDatabase'],
+            '--method':         self.param['targetDecoyMethod'],
+            '--decoyFormat':    self.param['decoyEntryPrefix'],
+            '--targetFormat':   self.param['targetEntryPrefix'], 
+            '--keepStart':      self.param['targetDecoyKeepStart'],
+            '--keepEnd':        self.param['targetDecoyKeepEnd']
+            }
+            command = self.binaryPath('ptb.decoyfasta')+' '
+            for fl,fi in opts.items():
+                command += fl+' "'+str(fi)+'" '
+            command += databases
+            os.system(command)
+            #print(command)
+            print("Done...")
+            
+if __name__ == '__main__':
+    script = DecoyFasta()
+
+
+"""
+{
+    function run()
+    {
+        if (isset($this->output->outputDatabase))
+        {
+            echo "Creating target/decoy database...\n";
+            $databases = "";
+            foreach ($this->input->databases as $path)
+                $databases .= ' "'.$path.'"';
+            $command = "{$this->binaryPath('ptb.decoyfasta')} --output \"{$this->output->outputDatabase}\" --method \"{$this->param->targetDecoyMethod}\" --keepStart {$this->param->targetDecoyKeepStart} --keepEnd {$this->param->targetDecoyKeepEnd} --targetFormat \"{$this->param->targetEntryPrefix}\" --decoyFormat \"{$this->param->decoyEntryPrefix}\" $databases";
+            passthru($command);
+            echo "done.\n";
+        }
+    }
+}
+
+$script = new DecoyFasta();
+"""
--- include/python/proteomatic.py
+++ include/python/proteomatic.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2.6
+#!/usr/bin/env python
 # encoding: utf-8
 """proteomatic.py class template 
 """
@@ -9,81 +9,118 @@ from copy import copy
 import subprocess
 import tempfile
 import json
-
+from pipes import quote
 
 
 class ProteomaticScript(object):
-	"""DocString to come
-	"""
-	def run(self):
-		raise NotImplementedError( "Should have implemented this" )
-	
-	def binaryPath(self,tool):
-		return self.anyLanguageHubResponse['binaryPath'][tool]
-	
-	def __init__(self):
-		commands = copy(sys.argv)
-		currentDir,scriptFilename = os.path.split(commands.pop(0))
-		currentDir = os.path.abspath(currentDir)
-		
-		completeScriptPath = os.path.join(currentDir,scriptFilename)
-		
-		os.chdir(currentDir)
-			
-		pathToRuby = "ruby"
-		if "--pathToRuby" in commands:
-			pos = commands.index("--pathToRuby")
-			commands.pop(pos)
-			pathToRuby = commands.pop(pos)
+    """DocString to come
+    """
+    def run(self):
+        raise NotImplementedError( "Should have implemented this" )
+    
+    def binaryPath(self,tool):
+        return self.anyLanguageHubResponse['binaryPath'][tool]
+    
+    def __init__(self):
+        commands = copy(sys.argv)
+        scriptDir,scriptFilename = os.path.split(commands.pop(0))
+        #currentDir = os.path.abspath(currentDir)
+        #completeScriptPath = os.path.join(currentDir,scriptFilename)
+        
+        #os.chdir(currentDir)
+            
+        pathToRuby = "ruby"
+        if "--pathToRuby" in commands:
+            pos = commands.index("--pathToRuby")
+            commands.pop(pos)
+            pathToRuby = commands.pop(pos)
 
-		pathToYamlDescription = os.path.abspath(os.path.join("include","properties","{0}.yaml".format(scriptFilename.split('.')[0])))
-		
-		controlFile  = tempfile.NamedTemporaryFile(mode='w',prefix='p-py-c-',delete=False)
-		controlFilePath = os.path.abspath(controlFile.name)
-		responseFile = tempfile.NamedTemporaryFile(mode='w',prefix='p-py-r-',delete=False)
-		responseFilePath =os.path.abspath(responseFile.name)
-		outputFile   = tempfile.NamedTemporaryFile(mode='w',prefix='p-py-o-',delete=False)
-		outputFilePath= os.path.abspath(outputFile.name)
-		controlFile.write("action: query\npathToYamlDescription: \"{0}\"\nresponseFilePath: \"{1}\"\nresponseFormat: json\n".format(pathToYamlDescription,responseFilePath))
-		controlFile.close()
-		responseFile.close()
-		outputFile.close()
+        pathToYamlDescription = os.path.join(scriptDir,"include","properties","{0}.yaml".format(scriptFilename.split('.')[0]))
+        
+        controlFile  = tempfile.NamedTemporaryFile(mode='w',prefix='p-py-c-',delete=False)
+        controlFilePath = controlFile.name
+        #controlFile.write("action: query\npathToYamlDescription: \"{0}\"\nresponseFilePath: \"{1}\"\nresponseFormat: json\n".format(pathToYamlDescription,responseFilePath))
+        controlFile.close()
+        
+        responseFile = tempfile.NamedTemporaryFile(mode='w',prefix='p-py-r-',delete=False)
+        responseFilePath =responseFile.name
+        responseFile.close()
+        
+        outputFile   = tempfile.NamedTemporaryFile(mode='w',prefix='p-py-o-',delete=False)
+        outputFilePath= outputFile.name
+        outputFile.close()
+        
+        argString = ''
+        for cmd in commands:
+            cmd = cmd.replace("\n", "\\n")
+            cmd = cmd.replace("\t", "\\t")
+            cmd = cmd.replace("\r", "\\r")
+            cmd = cmd.replace("\"", "\\\"")
+            argString += '  - "{0}"\n'.format(cmd)
+            
+        with open(controlFilePath,'w') as cf:
+            cf.write('action: query\n')
+            cf.write('pathToYamlDescription: "{0}"\n'.format(pathToYamlDescription.replace("\\","\\\\")))
+            cf.write('responseFilePath: "{0}"\n'.format(responseFilePath.replace("\\","\\\\")))
+            cf.write('responseFormat: json\n')
+            cf.write('arguments:\n{0}'.format(argString))
+        
+        hubPath = os.path.join(scriptDir,'helper','any-language-hub.rb')
+        command = "{0} {1} {2}".format(
+            quote(pathToRuby),
+            quote(hubPath),
+            quote(controlFilePath)
+        )
+        
+        os.system(command)
+        
+        responseFile = open(responseFilePath)
+        try:
+            self.anyLanguageHubResponse = json.load(responseFile)           
+            if 'run' in self.anyLanguageHubResponse.keys():
+                if self.anyLanguageHubResponse['run'] == 'run':
+                    outputFile = open(outputFilePath,'w',0)
+                    sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 0)
+                    os.dup2(sys.stdout.fileno(),outputFile.fileno())
+                
+                    #sys.stdout = outputFile
+                    self.param  = self.anyLanguageHubResponse['param']
+                    self.input  = self.anyLanguageHubResponse['input']
+                    self.output = self.anyLanguageHubResponse['output']
 
-		os.system( "\"{0}\" helper/any-language-hub.rb \"{1}\" {2} ".format(pathToRuby,controlFilePath, " ".join(["\"{0}\"".format(x) for x in commands])) )
-		
-		responseFile = open(responseFilePath)
-		try:
-			self.anyLanguageHubResponse = json.load(responseFile)			
-			if 'run' in self.anyLanguageHubResponse.keys():
-				if self.anyLanguageHubResponse['run'] == 'run':
-					outputFile = open(outputFilePath,'w',0)
-					sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 0)
-					os.dup2(sys.stdout.fileno(),outputFile.fileno())
-				
-					#sys.stdout = outputFile
-					self.param = self.anyLanguageHubResponse['param']
-					self.input = self.anyLanguageHubResponse['input']
-					self.output = self.anyLanguageHubResponse['output']
-
-					self.run()
-					outputFile.close()
-				
-					controlFile = open(controlFilePath, 'w')
-					controlFile.write("action: finish\npathToYamlDescription: \"{0}\"\nresponseFilePath: \"{1}\"\nresponseFormat: json\noutputFilePath: \"{2}\"\nstartTime: \"{3}\"\n".format(pathToYamlDescription,responseFilePath,outputFilePath,self.anyLanguageHubResponse['startTime']))
-					controlFile.close()
-				
-					os.system( "\"{0}\" helper/any-language-hub.rb \"{1}\" {2} ".format(pathToRuby,controlFilePath, " ".join(["\"{0}\"".format(x) for x in commands])) )
-		except:
-			pass #print("Something went senf-py-bogo ...")
-		responseFile.close()
-		os.unlink(controlFilePath)
-		os.unlink(responseFilePath)
-		os.unlink(outputFilePath)
+                    self.run()
+                    outputFile.close()
+                
+                    with open(controlFilePath,'w') as cf:
+                        cf.write('action: finish\n')
+                        cf.write('pathToYamlDescription: "{0}"\n'.format(pathToYamlDescription.replace("\\","\\\\")))
+                        cf.write('responseFilePath: "{0}"\n'.format(responseFilePath.replace("\\","\\\\")))
+                        cf.write('responseFormat: json\n')
+                        cf.write('arguments:\n{0}'.format(argString))
+                        cf.write('outputFilePath: "{0}"\n'.format(outputFilePath.replace("\\","\\\\")))
+                        cf.write('startTime: {0}\n'.format(self.anyLanguageHubResponse['startTime']))
+                    
+                    command = "{0} {1} {2}".format(
+                        quote(pathToRuby),
+                        quote(hubPath),
+                        quote(controlFilePath)
+                    )
+                    
+                    os.system( command )
+            else:
+                pass
+        except:
+            pass #print("Something went senf-py-bogo ...")
+        
+        responseFile.close()
+        os.unlink(controlFilePath)
+        os.unlink(responseFilePath)
+        os.unlink(outputFilePath)
 
 def main():
-	pass
+    pass
 
 
 if __name__ == '__main__':
-	main()
+    main()
 
