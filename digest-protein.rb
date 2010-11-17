6a9fe1acd5743097d49a7c20d4523f185621a896
Michael Specht <micha.specht@gmail.com>
2010-11-17 09:43:46 +0100

    added script for tryptic protein digestion

---
+++ digest-protein.rb
@@ -0,0 +1,47 @@
+# Copyright (c) 2007-2010 Michael Specht
+# 
+# This file is part of Proteomatic.
+# 
+# Proteomatic is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# Proteomatic is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with Proteomatic.  If not, see <http://www.gnu.org/licenses/>.
+
+require './include/ruby/proteomatic'
+require 'set'
+
+class DigestProtein < ProteomaticScript
+    def run()
+        protein = @param[:protein].dup
+        # remove invalid characters
+        protein.gsub!(/[^GASPVTCLINDQKEMHFRYW]/, '')
+        # insert spaces at tryptic cleavage sites
+        # this is done twice because after the first step,
+        # there might still be blocks like KK, KR, RK, or RR
+        2.times { protein.gsub!(/([RK])([^P])/, "\\1 \\2") }
+        parts = protein.split(' ')
+        peptides = Set.new
+        (0..@param[:mc]).each do |length|
+            (0...parts.size-length).each do |offset|
+                peptides << parts[offset, length + 1].join()
+            end
+        end
+        puts peptides.to_a.sort.join("\n")
+        # also write peptides to output file if requested
+        if @output[:results]
+            File::open(@output[:results], 'w') do |f|
+                f.puts peptides.to_a.sort.join("\n")
+            end
+        end
+    end
+end
+
+lk_Object = DigestProtein.new
